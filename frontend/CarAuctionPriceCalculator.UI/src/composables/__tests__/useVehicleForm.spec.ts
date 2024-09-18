cabd95d1a8cc21a90352f6c82655949098f1f958
Renan Silva <81625697+renanmoraisdasilva@users.noreply.github.com>
2024-09-18 09:33:07 -0300

    Refactor testing

--- frontend/CarAuctionPriceCalculator.UI/src/composables/__tests__/useVehicleForm.spec.ts
+++ frontend/CarAuctionPriceCalculator.UI/src/composables/__tests__/useVehicleForm.spec.ts
@@ -19,10 +19,6 @@ describe('useVehicleForm', () => {
   })
 
   it('should fetch vehicle types', async () => {
-    const mockVehicleTypes = [
-      { id: 1, name: 'Car' },
-      { id: 2, name: 'Truck' }
-    ]
     mockedAxios.get.mockResolvedValue({ data: mockVehicleTypes })
 
     await composable.fetchVehicleTypes()
@@ -31,10 +27,6 @@ describe('useVehicleForm', () => {
   })
 
   it('should calculate fees', async () => {
-    const mockFees = {
-      price: 398,
-      fees: [{ id: 1, feeType: { name: 'Basic' }, calculatedFee: 39.8 }]
-    }
     mockedAxios.post.mockResolvedValue({ data: mockFees })
 
     composable.basePrice.value = 398
@@ -42,7 +34,71 @@ describe('useVehicleForm', () => {
 
     await composable.fetchFees()
 
-    expect(composable.totalCost.value).toBe(398)
-    expect(composable.fees.value).toEqual([{ id: 1, name: 'Basic', amount: 39.8 }])
+    expect(composable.totalCost.value).toBe(550.76)
+    expect(composable.fees.value).toEqual([
+      {
+        id: 1,
+        name: 'Buyer',
+        amount: 39.8
+      },
+      {
+        id: 3,
+        name: 'Seller',
+        amount: 7.96
+      },
+      {
+        id: 5,
+        name: 'Association',
+        amount: 5
+      },
+      {
+        id: 9,
+        name: 'Storage',
+        amount: 100
+      }
+    ])
   })
 })
+
+const mockVehicleTypes = [
+  { id: 1, name: 'Car' },
+  { id: 2, name: 'Truck' }
+]
+
+const mockFees = {
+  price: 550.76,
+  fees: [
+    {
+      id: 1,
+      feeType: {
+        id: 1,
+        name: 'Buyer'
+      },
+      calculatedFee: 39.8
+    },
+    {
+      id: 3,
+      feeType: {
+        id: 2,
+        name: 'Seller'
+      },
+      calculatedFee: 7.96
+    },
+    {
+      id: 5,
+      feeType: {
+        id: 3,
+        name: 'Association'
+      },
+      calculatedFee: 5
+    },
+    {
+      id: 9,
+      feeType: {
+        id: 4,
+        name: 'Storage'
+      },
+      calculatedFee: 100
+    }
+  ]
+}
